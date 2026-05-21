014a16cba22b8d233cfa30a752f87951652ad751
BODURUDDIN <117629880+boduruddin84@users.noreply.github.com>
2026-05-21 11:51:37 +0600

    Mongodb connect

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const dotenv = require('dotenv');
 const cors = require('cors');
 dotenv.config();
@@ -8,7 +9,35 @@ const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
+}); 
+
+
+
+const uri = "mongodb+srv://idea-vault-server:KbCYjJORZkyGkjTV@cluster0.cr3aexn.mongodb.net/?appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
